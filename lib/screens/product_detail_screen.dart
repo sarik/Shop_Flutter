2c128672cbe0d4488560a88f61249d8fd46d6378
Sarik Siddiqui <user2@users-MacBook-Pro.local>
2019-11-10 02:48:42 +0530

    added body to product detail screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/products.dart';
-
+import '../providers/cart.dart';
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
   // final double price;
@@ -18,9 +18,52 @@ class ProductDetailScreen extends StatelessWidget {
       context,
       listen: false,
     ).findById(productId);
+
+    final cart = Provider.of<Cart>(
+      context,
+      listen: false,
+    );
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: 400,
+            margin: EdgeInsets.fromLTRB(0, 40, 0, 0),
+            child: Image.network(loadedProduct.imageUrl, fit: BoxFit.contain),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            loadedProduct.description,
+            style: TextStyle(color: Colors.grey),
+            textAlign: TextAlign.center,
+            softWrap: true,
+          ),
+          Text(
+            "\$"+loadedProduct.price.toString(),
+            style: TextStyle(color: Colors.grey),
+            textAlign: TextAlign.center,
+            softWrap: true,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text( cart.cartItems.containsKey(loadedProduct.id)  ? "Added to cart": "Add to cart"),
+              IconButton(
+                icon: Icon(
+                  Icons.shopping_cart,
+                ),
+                onPressed: () {cart.addToCart(loadedProduct.id, loadedProduct.price, loadedProduct.title);},
+               color: Theme.of(context).accentColor,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
